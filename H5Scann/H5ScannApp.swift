9c74bbcfa1e79cf794605e10c51ad9f0d52b5824
1024小神 <15670339118@163.com>
2025-03-18 18:53:06 +0800

    ad

---
+++ H5Scann/H5ScannApp.swift
@@ -0,0 +1,32 @@
+//
+//  H5ScannApp.swift
+//  H5Scann
+//
+//  Created by song on 2025/3/18.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct H5ScannApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
